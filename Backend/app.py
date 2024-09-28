25ab3f7ce730930df8d43dbf21b22cf3de6a3395
Nihara Padil <niharapadilhr@gmail.com>
2024-09-28 22:05:25 +0530

    working

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)  # Allow requests from localhost:3000
 
 # Load the trained model
 model = pickle.load(open('model/model.pkl', 'rb'))
